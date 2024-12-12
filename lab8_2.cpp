9f2b85de9b7c6cca799a1f54c1ef6e4a2547ee51
thanpisit-film-hub <tanpisit.2548@gmail.com>
2024-12-12 19:44:12 +0700

    Lab8

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -3,7 +3,25 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade( double x){
+    char g;
+    if(x > 90){
+        g ='A';
+    }
+    if(x > 75 and x <= 90){
+        g = 'B';
+    }
+    if(x > 60 and x <= 75){
+        g = 'C';
+    }
+    if(x > 45 and x <= 60){
+        g = 'D';
+    }
+    if(x <= 45){
+        g = 'F';
+    }
+    return g;
+}
 
 int main(){
 	//Input the number of students
@@ -17,9 +35,9 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin, name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
 	
--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,40 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<string>
+
+int main(){
+    
+    int _age, _height, _bounty;
+    std::cout << "Enter your age: ";
+    std::cin >> _age;
+    if(_age <= 25){
+        std::cout << "Enter your height: ";
+        std::cin >> _height;
+        if(_height < 100){
+            std::cout << "Your character = " << "Chopper";
+        }
+        if(_height < 180 and _height >= 100){
+            std::cout << "Your character = " << "Usopp";
+        }
+        if(_height >= 180){
+            std::cout << "Enter your bounty: ";
+            std::cin >> _bounty;
+            if(_bounty <= 1100000000){
+                std::cout << "Your character = " << "Sanji";
+            }else{
+                std::cout << "Your character = " << "Zoro";
+            }
+        }
+    }
+    if(_age > 25 and _age <= 60){
+        std::cout << "Enter your bounty: ";
+        std::cin >> _bounty;
+        if(_bounty <= 500000000){
+                std::cout << "Your character = " << "Franky";
+        }else{
+            std::cout << "Your character = " << "Jinbe";
+        }
+    }
+    if(_age > 60){
+        std::cout << "Your character = " << "Brook";
+    }
+}
